c52c6a236a5b256e98f509613e966918c4b22269
Spudowski <156944179+Spudowski@users.noreply.github.com>
2024-04-25 20:13:38 +0200

    hw2

--- goit-js-hw-01/js/task-1.js
+++
@@ -1,7 +0,0 @@
-function makeTransaction(quantity, pricePerDroid) {
-    console.log(`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`)
-}
-
-makeTransaction(5, 3000);
-makeTransaction(3, 1000);
-makeTransaction(10, 500);
--- goit-js-hw-01/js/task-2.js
+++
@@ -1,7 +0,0 @@
-function getShippingMessage(country, price, deliveryFee) {
-    console.log(`Shipping to ${country} will cost ${price + deliveryFee} credits`)
-}
-
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
